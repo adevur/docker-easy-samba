31f47bd58a37d00b5e2da45f9b16dd7ec4a46923
adevur <madavurro@protonmail.com>
2019-04-18 18:01:01 +0200

    Implemented validation of users' passwords defined in config file

--- development/startup/functions/fnIsValidPassword.js
+++ development/startup/functions/fnIsValidPassword.js
@@ -7,11 +7,18 @@ module.exports = fnIsValidPassword;
 
 
 // dependencies
-// N/A
+const fnIsString = require("/startup/functions/fnIsString.js");
+const fnIsPrintableString = require("/startup/functions/fnIsPrintableString.js");
 
 
 
-// TODO: implement
+// FUNCTION: fnIsValidPassword()
+// INPUT: "password" is a javascript object
+// OUTPUT: true in case of success, otherwise false
+// PURPOSE: this function returns true in case "password" is a valid Linux password
+//   EXPLAIN: "password" must a string, must have a minimum length of 1 char,
+//     and can only contain printable ASCII chars
 function fnIsValidPassword(password){
-    return true;
+    // "password" must be a string, of minimum 1 char, made entirely of printable ASCII chars
+    return (fnIsString(password) && password.length >= 1 && fnIsPrintableString(password));
 }
