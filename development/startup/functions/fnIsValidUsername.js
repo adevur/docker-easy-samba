ce91f776b0ca19a6ed819976d68947388bf0191f
adevur <madavurro@protonmail.com>
2019-06-02 22:30:59 +0200

    Code cleanup

--- development/startup/functions/fnIsValidUsername.js
+++ development/startup/functions/fnIsValidUsername.js
@@ -8,11 +8,12 @@ module.exports = fnIsValidUsername;
 
 // dependencies
 const fnValidateString = require("/startup/functions/fnValidateString.js");
+const fnIsString = require("/startup/functions/fnIsString.js");
 
 
 
 // FUNCTION: fnIsValidUsername()
-// INPUT: "username" is the string to validate
+// INPUT: "username" is the javascript object to validate
 // OUTPUT: true in case "username" is a valid UNIX username, otherwise false
 // PURPOSE: check that a given string is a valid UNIX username
 // EXPLAIN: for this script, valid username rules are:
@@ -21,6 +22,11 @@ const fnValidateString = require("/startup/functions/fnValidateString.js");
 //   username body can contain lowercase letters, digits, underscores and hyphens
 //   username last char can also be a dollar sign
 function fnIsValidUsername(username){
+    // username must be a string
+    if (fnIsString(username) !== true){
+        return false;
+    }
+
     // username must have a length of minimum 1 char and maximum 31 chars
     if (username.length < 1 || username.length > 31){
         return false;
