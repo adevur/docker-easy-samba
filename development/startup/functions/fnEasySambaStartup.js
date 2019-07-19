fe453136153ac10ffeebb659f1edf2582ae6d789
adevur <madavurro@protonmail.com>
2019-07-19 12:50:33 +0200

    Code cleanup

--- development/startup/functions/fnEasySambaStartup.js
+++ development/startup/functions/fnEasySambaStartup.js
@@ -8,11 +8,11 @@ module.exports = fnEasySambaStartup;
 
 // dependencies
 const fs = require("fs");
-const { spawn } = require("child_process");
 const fnIsRunning = require("/startup/functions/fnIsRunning.js");
 const fnSleep = require("/startup/functions/fnSleep.js");
 const fnDeleteFile = require("/startup/functions/fnDeleteFile.js");
 const fnGetVersion = require("/startup/functions/fnGetVersion.js");
+const fnSpawn = require("/startup/functions/fnSpawn.js");
 
 
 
@@ -26,26 +26,21 @@ async function fnEasySambaStartup(){
 
     // start EasySamba Remote API daemon
     //   only in case "/share/config.json" and "/share/config.gen.js" files are missing
-    try {
-        if (fnIsRunning("node /startup/remote-api/index.js") !== true && fs.existsSync("/share/config.json") !== true && fs.existsSync("/share/config.gen.js") !== true){
-            fnDeleteFile("/startup/remote-api.started");
-
-            spawn("node", ["/startup/remote-api/index.js"], { stdio: "ignore" })
-                .on("error", () => {
-                    // do nothing
-                })
-                .on("exit", () => {
-                    // do nothing
-                })
-            ;
-
-            await fnSleep(2000);
-
+    if (fs.existsSync("/share/config.json") !== true && fs.existsSync("/share/config.gen.js") !== true){
+        try {
+            if (fnIsRunning("node /startup/remote-api/index.js") !== true){
+                fnDeleteFile("/startup/remote-api.started");
+                fnSpawn("node", ["/startup/remote-api/index.js"]);
+                await fnSleep(2000);
+            }
             console.log((fs.existsSync("/startup/remote-api.started")) ? `[LOG] EasySamba Remote API started successfully.` : `[ERROR] it's not been possible to start EasySamba Remote API.`);
         }
+        catch (error){
+            console.log(`[ERROR] it's not been possible to start EasySamba Remote API.`);
+        }
     }
-    catch (error){
-        // do nothing
+    else {
+        console.log(`[LOG] EasySamba Remote API is disabled.`);
     }
 
     // delete "/startup/easy-samba.running"
