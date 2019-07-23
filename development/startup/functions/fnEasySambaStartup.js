e92407e1f9c0f19316a080054189ae4b41a1e2ef
adevur <madavurro@protonmail.com>
2019-07-23 11:46:35 +0200

    Improved startup procedure

--- development/startup/functions/fnEasySambaStartup.js
+++ development/startup/functions/fnEasySambaStartup.js
@@ -13,6 +13,7 @@ const fnWriteFile = require("/startup/functions/fnWriteFile.js");
 const fnGetVersion = require("/startup/functions/fnGetVersion.js");
 const fnListUsers = require("/startup/functions/fnListUsers.js");
 const fnGetConfigDir = require("/startup/functions/fnGetConfigDir.js");
+const fnKill = require("/startup/functions/fnKill.js");
 
 
 
@@ -32,6 +33,12 @@ async function fnEasySambaStartup(){
     fnWriteFile("/startup/configdir.json", JSON.stringify(configdir));
     console.log(`[LOG] easy-samba configuration files are located at "${configdir}".`);
 
+    // kill existing processes
+    fnKill("node /share/config.gen.js");
+    fnKill("node /share/config/config.gen.js");
+    fnKill("/usr/sbin/smbd --foreground --no-process-group");
+    fnKill("/usr/sbin/nmbd --foreground --no-process-group");
+
     // delete "/startup/remote-api.started"
     fnDeleteFile("/startup/remote-api.started");
 
@@ -39,9 +46,8 @@ async function fnEasySambaStartup(){
     fnDeleteFile("/startup/easy-samba.running");
     
     // in case of first startup, save a list of container's OS native users
-    if (fs.existsSync("/startup/first_startup") === true){
+    if (fs.existsSync("/startup/native_users.json") !== true){
         fnWriteFile("/startup/native_users.json", JSON.stringify(fnListUsers()));
-        fnDeleteFile("/startup/first_startup");
     }
     
     console.log("------ EASY-SAMBA STARTUP COMPLETE ------\n");
