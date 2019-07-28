3661bc15a3ed8ac4c7bee013daa59ba1c77b2330
adevur <madavurro@protonmail.com>
2019-07-28 17:25:15 +0200

    Improved fnCleanUpUsers()

--- development/startup/functions/fnCleanUpUsers.js
+++ development/startup/functions/fnCleanUpUsers.js
@@ -7,10 +7,13 @@ module.exports = fnCleanUpUsers;
 
 
 // dependencies
+const fs = require("fs");
 const { spawnSync } = require("child_process");
+const assert = require("assert");
 const fnListUsers = require("/startup/functions/fnListUsers.js");
 const fnListSambaUsers = require("/startup/functions/fnListSambaUsers.js");
-const fs = require("fs");
+const fnIsArray = require("/startup/functions/fnIsArray.js");
+const fnIsString = require("/startup/functions/fnIsString.js");
 
 
 
@@ -19,9 +22,11 @@ function fnCleanUpUsers(){
     try {
         // retrieve the list of CentOS native users from file "/startup/native_users.json", created on first startup
         const nativeUsers = JSON.parse(fs.readFileSync("/startup/native_users.json", "utf8"));
+        assert( fnIsArray(nativeUsers) && nativeUsers.every(fnIsString) );
 
         // get current users' list
         const currentUsers = fnListUsers();
+        assert( nativeUsers.every((e) => { return currentUsers.includes(e); }) );
 
         // delete each user not included in 'nativeUsers', both from OS and from SAMBA
         currentUsers.forEach((user) => {
