7b4168365d987c278e99f90fc3ba0c3a0ee8ad68
adevur <madavurro@protonmail.com>
2019-08-01 18:35:08 +0200

    Initial support for Remote API command 'pause-easy-samba'

--- development/startup/functions/fnEasySambaLoop.js
+++ development/startup/functions/fnEasySambaLoop.js
@@ -30,136 +30,212 @@ const CFG = require("/startup/functions/fnGetConfigDir.js")();
 // INPUT: N/A
 // OUTPUT: N/A
 async function fnEasySambaLoop(){
-    let counter = 0;
-    let previousConfig = undefined;
-    let shares = undefined;
+    const vars = {
+        counter: 0,
+        previousConfig: undefined,
+        shares: undefined,
+        paused: false,
+        pausedAware: false
+    };
 
     // loop every 10 seconds
     while (true){
-        // in case someone used Remote API "stop-easy-samba"
-        if (fs.existsSync("/startup/easy-samba.stop")){
-            let message = "";
-            try {
-                message = JSON.parse( fs.readFileSync("/startup/easy-samba.stop", "utf8") );
-                assert( fnIsString(message) );
-            }
-            catch (error){
-                message = "";
-            }
-            try {
-                log(`[WARNING] easy-samba is being stopped via Remote API.`);
-                if (message.length > 0){
-                    log(`[LOG] ${message}`);
-                }
-            }
-            catch (error){
-                // do nothing
-            }
-            fnKill("/usr/sbin/smbd --foreground --no-process-group");
-            fnKill("/usr/sbin/nmbd --foreground --no-process-group");
-            fnKill(`node ${CFG}/config.gen.js`);
-            fnKill("node /startup/remote-api/index.js");
-            process.exit(0);
-        }
-    
-        // execute "config.gen.js" in case "config.json" is missing
-        if (fs.existsSync(`${CFG}/config.json`) !== true && fs.existsSync(`${CFG}/config.gen.js`) === true){
-            if (fnIsRunning(`node ${CFG}/config.gen.js`) !== true){
-                log(`[LOG] executing '${CFG}/config.gen.js'...\n`);
-                fnSpawn("node", [`${CFG}/config.gen.js`]);
-                await fnSleep(2000);
-            }
-        }
-        // kill "config.gen.js" in case it doesn't exist
-        else if (fs.existsSync(`${CFG}/config.gen.js`) !== true){
-            fnKill(`node ${CFG}/config.gen.js`);
-        }
-
-        // start EasySamba Remote API
-        await fnStartRemoteAPI();
-
-        // load configuration
-        const { config, rawConfig } = fnLoadConfig();
-
-        // check for changes
-        let somethingChanged = false;
-        let sambaCrashed = false;
-        if (rawConfig !== false && rawConfig !== previousConfig){
-            somethingChanged = true;
-        }
-        if (previousConfig !== undefined && (fnIsRunning("/usr/sbin/nmbd --foreground --no-process-group") !== true || fnIsRunning("/usr/sbin/smbd --foreground --no-process-group") !== true)){
-            sambaCrashed = true;
-        }
-
-        // update running configuration,
-        //   in case it's first startup, something changed, or SAMBA crashed
-        if (previousConfig === undefined || somethingChanged || sambaCrashed){
-            log(`------ EASY-SAMBA CONFIGURATION PROCESS #${counter.toString()} ------`);
-            let res = false;
-            
-            shares = undefined;
-            
-            // if SAMBA crashed, show a warning
-            if (sambaCrashed){
-                log(`[WARNING] 'smbd' or 'nmbd' crashed unexpectedly.`);
-            }
-            
-            if (previousConfig !== undefined || config !== false){
-                log(`[LOG] SAMBA server configuration process has started.`);
-            }
-            
-            if (previousConfig === undefined && config === false){
-                log(`[ERROR] easy-samba configuration file could not be loaded or it is not in JSON format.`);
-                res = false;
-            }
-            else if (previousConfig === undefined && config !== false){
-                res = await fnUpdateConfig(config);
-                previousConfig = rawConfig;
-            }
-            else if (previousConfig !== undefined && config === false){
-                res = await fnUpdateConfig(JSON.parse(previousConfig));
-            }
-            else if (previousConfig !== undefined && config !== false){
-                res = await fnUpdateConfig(config);
-                previousConfig = rawConfig;
-            }
-            
-            // in case configuration updated successfully
-            if (fnHas(res, "shares")){
-                fnWriteFile("/startup/easy-samba.running");
-                shares = res.shares;
-            }
-            // in case it's not been possible to update configuration
-            else {
-                fnDeleteFile("/startup/easy-samba.running");
-                fnKill("/usr/sbin/smbd --foreground --no-process-group");
-                fnKill("/usr/sbin/nmbd --foreground --no-process-group");
-                log(`[WARNING] configuration process has failed, re-trying in 10 seconds.`);
-                previousConfig = undefined;
-                shares = undefined;
-            }
-
-            log(`------ EASY-SAMBA CONFIGURATION PROCESS FINISHED ------\n`);
-            counter += 1;
-        }
-        
-        // apply soft-quota
-        if (shares !== undefined && fs.existsSync("/startup/easy-samba.running")){
-            // if there's at least one shared folder with soft-quota
-            if (shares.some((e) => { return fnHas(e, "$soft-quota"); })){
-                try {
-                    assert( fnCreateShares(shares) === true );
-                }
-                catch (error){
-                    log("[WARNING] it's not been possible to apply soft-quota to shared folders.\n");
-                }
-            }
-        }
-        
+        await fnEasySambaCycle(vars);
         await fnSleep(10000);
     }
 
     return;
+}
+
+
+
+async function fnEasySambaCycle(vars){
+    // in case someone used Remote API "stop-easy-samba"
+    fnEasySambaCycleManageStop();
+    
+    // in case someone has "paused" easy-samba via Remote API "pause-easy-samba"
+    fnEasySambaCycleManagePause(vars);
+
+    // manage "config.gen.js" file
+    await fnEasySambaCycleManageConfigGen(vars);
+
+    // start EasySamba Remote API
+    await fnStartRemoteAPI();
+
+    // start easy-samba configuration process
+    await fnEasySambaCycleProcess(vars);
+    
+    // apply soft-quota
+    fnEasySambaCycleQuota(vars);
+}
+
+
+
+function fnEasySambaCycleManageStop(){
+    if (fs.existsSync("/startup/easy-samba.stop")){
+        let message = "";
+        try {
+            message = JSON.parse( fs.readFileSync("/startup/easy-samba.stop", "utf8") );
+            assert( fnIsString(message) );
+        }
+        catch (error){
+            message = "";
+        }
+        try {
+            log(`[WARNING] easy-samba is being stopped via Remote API.`);
+            if (message.length > 0){
+                log(`[LOG] ${message}`);
+            }
+        }
+        catch (error){
+            // do nothing
+        }
+        fnKill("/usr/sbin/smbd --foreground --no-process-group");
+        fnKill("/usr/sbin/nmbd --foreground --no-process-group");
+        fnKill(`node ${CFG}/config.gen.js`);
+        fnKill("node /startup/remote-api/index.js");
+        process.exit(0);
+    }
+}
+
+
+
+function fnEasySambaCycleManagePause(vars){
+    if (fs.existsSync("/startup/easy-samba.pause")){
+        vars["paused"] = true;
+    }
+    else {
+        if (vars["paused"] === true){
+            log(`[LOG] easy-samba has been resumed from pause, and it will now restart again.\n`);
+        }
+        vars["paused"] = false;
+        vars["pausedAware"] = false;
+    }
+}
+
+
+
+async function fnEasySambaCycleManageConfigGen(vars){
+    // if easy-samba is paused, kill "config.gen.js"
+    if (vars["paused"] === true){
+        fnKill(`node ${CFG}/config.gen.js`);
+    }
+    // execute "config.gen.js" in case "config.json" is missing
+    else if (fs.existsSync(`${CFG}/config.json`) !== true && fs.existsSync(`${CFG}/config.gen.js`) === true){
+        if (fnIsRunning(`node ${CFG}/config.gen.js`) !== true){
+            log(`[LOG] executing '${CFG}/config.gen.js'...\n`);
+            fnSpawn("node", [`${CFG}/config.gen.js`]);
+            await fnSleep(2000);
+        }
+    }
+    // kill "config.gen.js" in case it doesn't exist
+    else if (fs.existsSync(`${CFG}/config.gen.js`) !== true){
+        fnKill(`node ${CFG}/config.gen.js`);
+    }
+}
+
+
+
+async function fnEasySambaCycleProcess(vars){
+    // if easy-samba is paused, and we are already aware of that,
+    //   we do nothing
+    if (vars["paused"] === true && vars["pausedAware"] === true){
+        return;
+    }
+    // if easy-samba has been paused and we're not aware of it yet,
+    //   we stop everything
+    else if (vars["paused"] === true && vars["pausedAware"] !== true){
+        fnDeleteFile("/startup/easy-samba.running");
+        fnKill("/usr/sbin/smbd --foreground --no-process-group");
+        fnKill("/usr/sbin/nmbd --foreground --no-process-group");
+        vars["previousConfig"] = undefined;
+        vars["shares"] = undefined;
+        log(`[WARNING] easy-samba has been paused via Remote API. It will not restart until you send command 'start-easy-samba' via Remote API.\n`);
+        vars["pausedAware"] = true;
+        return;
+    }
+
+    // load configuration
+    const { config, rawConfig } = fnLoadConfig();
+
+    // check for changes
+    let somethingChanged = false;
+    let sambaCrashed = false;
+    let easysambaPaused = false;
+    if (rawConfig !== false && rawConfig !== vars["previousConfig"]){
+        somethingChanged = true;
+    }
+    if (vars["previousConfig"] !== undefined && (fnIsRunning("/usr/sbin/nmbd --foreground --no-process-group") !== true || fnIsRunning("/usr/sbin/smbd --foreground --no-process-group") !== true)){
+        sambaCrashed = true;
+    }
+
+    // update running configuration,
+    //   in case it's first startup, something changed, or SAMBA crashed
+    if (vars["previousConfig"] === undefined || somethingChanged || sambaCrashed){
+        log(`------ EASY-SAMBA CONFIGURATION PROCESS #${vars["counter"].toString()} ------`);
+        let res = false;
+        
+        vars["shares"] = undefined;
+        
+        // if SAMBA crashed, show a warning
+        if (sambaCrashed){
+            log(`[WARNING] 'smbd' or 'nmbd' crashed unexpectedly.`);
+        }
+        
+        if (vars["previousConfig"] !== undefined || config !== false){
+            log(`[LOG] SAMBA server configuration process has started.`);
+        }
+        
+        if (vars["previousConfig"] === undefined && config === false){
+            log(`[ERROR] easy-samba configuration file could not be loaded or it is not in JSON format.`);
+            res = false;
+        }
+        else if (vars["previousConfig"] === undefined && config !== false){
+            res = await fnUpdateConfig(config);
+            vars["previousConfig"] = rawConfig;
+        }
+        else if (previousConfig !== undefined && config === false){
+            res = await fnUpdateConfig(JSON.parse(vars["previousConfig"]));
+        }
+        else if (vars["previousConfig"] !== undefined && config !== false){
+            res = await fnUpdateConfig(config);
+            vars["previousConfig"] = rawConfig;
+        }
+        
+        // in case configuration updated successfully
+        if (fnHas(res, "shares")){
+            fnWriteFile("/startup/easy-samba.running");
+            vars["shares"] = res.shares;
+        }
+        // in case it's not been possible to update configuration
+        else {
+            fnDeleteFile("/startup/easy-samba.running");
+            fnKill("/usr/sbin/smbd --foreground --no-process-group");
+            fnKill("/usr/sbin/nmbd --foreground --no-process-group");
+            vars["previousConfig"] = undefined;
+            vars["shares"] = undefined;
+            log(`[WARNING] configuration process has failed, re-trying in 10 seconds.`);
+        }
+
+        log(`------ EASY-SAMBA CONFIGURATION PROCESS FINISHED ------\n`);
+        vars["counter"] += 1;
+    }
+}
+
+
+
+function fnEasySambaCycleQuota(vars){
+    if (vars["shares"] !== undefined && fs.existsSync("/startup/easy-samba.running")){
+        // if there's at least one shared folder with soft-quota
+        if (vars["shares"].some((e) => { return fnHas(e, "$soft-quota"); })){
+            try {
+                assert( fnCreateShares(vars["shares"]) === true );
+            }
+            catch (error){
+                log("[WARNING] it's not been possible to apply soft-quota to shared folders.\n");
+            }
+        }
+    }
 }
 
 
